ab8cfa1b739852dd1ff09fd26bbed5c34da5d873
Markus Wendorf <markuswendorf@web.de>
2022-06-15 23:44:01 +0200

    things

--- react/generic-component/install.sh
+++ react/generic-component/install.sh
@@ -4,6 +4,6 @@ npm i -D prettier-plugin-tailwindcss
 npm i -D eslint eslint-config-prettier
 npm i -D @typescript-eslint/eslint-plugin
 
-# React specific
+# React specific plugin
 npm i -D eslint-plugin-react
 
